6c33bc48a0e795533fa618a293e348a26101040f
Tetiana <tetyana.fomscheva@mail.ru>
2016-12-13 15:20:32 +0000

    Initial

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// JavaScript File
+ //for (var i = 10; i >=0; i--) {
+ //  document.write (i);
+//   document.write ('<br>');
+//}
+
+// for (var i = 10; i>=0; i=i-2) {
+ //  document.write (i);
+//   document.write ('<br>');
+//}
+//var i = 0;
+//while (i<=10) {
+ 
+ //  document.write (i);
+//   document.write ('<br>');
+//   i++;
+//}
+// for (var i = 0; i<=10; i=i-2) {
+ // if (i%2 ==0)
+ //  document.write (i);
+ //  document.write ('<br>');
+//}
+var i = 0;
+do {
+ 
+   document.write (i);
+   document.write ('<br>');
+   i++;
+} 
+   while (i<=10)
